86cb642342446af60860d36435d119a21b137388
Manuel Diaz <yosoymacnolo@gmail.com>
2024-05-07 16:40:48 -0600

    feat(accounts): create accounts

---
+++ lib/authy_web/controllers/user_json.ex
@@ -0,0 +1,25 @@
+defmodule AuthyWeb.UserJSON do
+  alias Authy.Accounts.User
+
+  @doc """
+  Renders a list of users.
+  """
+  def index(%{users: users}) do
+    %{data: for(user <- users, do: data(user))}
+  end
+
+  @doc """
+  Renders a single user.
+  """
+  def show(%{user: user}) do
+    %{data: data(user)}
+  end
+
+  defp data(%User{} = user) do
+    %{
+      id: user.id,
+      email: user.email,
+      hashed_password: user.hashed_password
+    }
+  end
+end
